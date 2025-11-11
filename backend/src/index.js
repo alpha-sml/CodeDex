0a740bdfb119a26195d5fc3cb8f47bac781d1383
alpha-sml <sml.atharva.14@gmail.com>
2025-11-12 01:17:08 +0530

    cors changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [`http://${process.env.FRONTEND_URL}`],
     credentials: true
 }));
 app.use(express.json());
